7310b39df761a0ab813ee563a136cddb3617c65a
sergeikabuldzhanov <thesiegebot@gmail.com>
2019-12-10 18:50:46 +0500

    Refactored project to use context API

--- reducer-todo/src/App.js
+++ reducer-todo/src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import './App.css';
-import TodoList from './components/TodoList';
+import React, {useReducer} from "react";
+import "./App.css";
+import TodoList from "./components/TodoList";
+import TodoContext from "./contexts/TodoContext";
+import { reducer, initialState } from "./reducers";
+
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleSubmit = name => event => {
+    event.preventDefault();
+    dispatch({ type: "ADD_TODO", payload: name });
+  };
+
+  const handleToggle = id => event => {      
+    dispatch({ type: "TOGGLE_TODO", payload: id });
+  };
+
+  const clearCompleted = event=>{
+      dispatch({type: "CLEAR_COMPLETED"});
+  }
   return (
     <div className="App">
-      <TodoList/>
+      <TodoContext.Provider value = {{...state, handleSubmit, handleToggle, clearCompleted}}>
+        <TodoList />
+      </TodoContext.Provider>
     </div>
   );
 }
--- reducer-todo/src/components/TodoList.js
+++ reducer-todo/src/components/TodoList.js
@@ -1,26 +1,14 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useContext } from "react";
 import { reducer, initialState } from "../reducers";
 import TodoForm from "./TodoForm";
+import TodoContext from '../contexts/TodoContext';
 
 export default function TodoList(props) {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  const handleSubmit = name => event => {
-    event.preventDefault();
-    dispatch({ type: "ADD_TODO", payload: name });
-  };
-
-  const handleToggle = id => event => {      
-    dispatch({ type: "TOGGLE_TODO", payload: id });
-  };
-
-  const clearCompleted = event=>{
-      dispatch({type: "CLEAR_COMPLETED"});
-  }
+    const {todos, handleSubmit, handleToggle, clearCompleted} = useContext(TodoContext);
 
   return (
     <div className="todo-list">
-      {state.todos.map(element => (
+      {todos.map(element => (
         <p
           style = {{textDecoration: element.completed ? "line-through" : "none"}} 
           onClick={handleToggle(element.id)} key={element.id}>
